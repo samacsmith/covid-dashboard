52e09dfeee887eb6a30cd5e590412b89e5dc83ba
Sam Smith <samuelacsmith@aol.com>
2021-01-27 23:33:49 +0000

    update bar chart legend again

--- get_data.py
+++ get_data.py
@@ -206,7 +206,7 @@ def make_bar(df, x_title, y_title, x, y, rolling_avg):
     fig=px.bar(df, x=x, y=y,
              labels={x: x_title, y: y_title})
     
-    fig.update_traces(marker_color=colors['maincolor'], hovertemplate='Daily: %{y:,.0f}', howlegend=True)
+    fig.update_traces(name=f'Daily Doses', marker_color=colors['maincolor'], hovertemplate='Daily: %{y:,.0f}', showlegend=True)
 
     fig2 = px.line(df, x=x, y='daily_rolling_average', line_shape='spline')
     fig2.update_traces(name=f'{rolling_avg} day rolling average', showlegend=True, line_color='#000000', hovertemplate='%{y:,.0f}')
